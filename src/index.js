2b724a3f08746e8a3a56cc40d7a3802b0397dd4c
Vivian Sarazin <vivian.sarazin.dev@gmail.com>
2021-03-06 17:12:42 +0100

    feat: adding BTC wallet

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const key = process.env.KRAKEN_API_KEY // API Key
 const secret = process.env.KRAKEN_SECRET_KEY // API Private Key
 const KrakenClient = require('kraken-api')
@@ -9,65 +10,51 @@ async function wait() {
 
 const configs = [
   {
-    ticker: 'XXBTZEUR',
+    ticker: 'XETHXXBT',
     amount: 15,
     priceDecimals: 1,
     volumeDecimals: 8,
   },
   {
-    ticker: 'XETHZEUR',
+    ticker: 'XLTCXXBT',
     amount: 15,
-    priceDecimals: 1,
-    volumeDecimals: 8,
-  },
-  {
-    ticker: 'ADAEUR',
-    amount: 30,
     priceDecimals: 6,
-    volumeDecimals: 6,
-  },
-  {
-    ticker: 'DOTEUR',
-    amount: 15,
-    priceDecimals: 4,
     volumeDecimals: 6,
   },
 ]
 
-;(async () => {
-  // Display user's balance
-  let eurBalance = (await kraken.api('Balance')).result.ZEUR
-  console.log('eurBalance', eurBalance)
+  ; (async () => {
+    // Display user's balance
+    let btcBalance = (await kraken.api('Balance')).result.XXBT
+    // Get Ticker Info
+    const tickerInfo = await kraken.api('Ticker', { pair: configs.map(pair => pair.ticker).join(',') })
 
-  // Get Ticker Info
-  const tickerInfo = await kraken.api('Ticker', { pair: configs.map(pair => pair.ticker).join(',') })
+    configs.forEach(pair => {
+      pair.currentPrice = parseFloat(tickerInfo.result[pair.ticker].a[0])
+    })
 
-  configs.forEach(pair => {
-    pair.currentPrice = parseFloat(tickerInfo.result[pair.ticker].a[0])
-  })
-
-  for (const pair of configs) {
-    const volume = (pair.amount / pair.currentPrice).toFixed(pair.volumeDecimals)
-    const price = pair.currentPrice.toFixed(pair.priceDecimals)
-    console.log(`Trying to place order. ${volume} ${pair.ticker} @ ${price}€ (= ${pair.amount} €)`)
-    if (eurBalance - pair.amount < 0) {
-      console.log('Insufficient funds, abort.')
-      break
+    for (const pair of configs) {
+      const volume = (pair.amount / pair.currentPrice).toFixed(pair.volumeDecimals)
+      const price = pair.currentPrice.toFixed(pair.priceDecimals)
+      console.log(`Trying to place order. ${volume} ${pair.ticker} @ ${price}€ (= ${pair.amount} €)`)
+      if (btcBalance - pair.amount < 0) {
+        console.log('Insufficient funds, abort.')
+        break
+      }
+      try {
+        const result = await kraken.api('AddOrder', {
+          pair: pair.ticker,
+          type: 'buy',
+          ordertype: 'limit',
+          price,
+          volume,
+          expiretm: `+${60 * 60 * 24}`,
+        })
+        btcBalance -= pair.amount
+        console.log(result.result.descr)
+      } catch (err) {
+        console.error(err)
+      }
+      await wait()
     }
-    try {
-      const result = await kraken.api('AddOrder', {
-        pair: pair.ticker,
-        type: 'buy',
-        ordertype: 'limit',
-        price,
-        volume,
-        expiretm: `+${60 * 60 * 24}`,
-      })
-      eurBalance -= pair.amount
-      console.log(result.result.descr)
-    } catch (err) {
-      console.error(err)
-    }
-    await wait()
-  }
-})()
+  })()
